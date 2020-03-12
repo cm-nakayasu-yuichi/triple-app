1bb8b05587d6f4954d10123148d792175239dd4a
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2020-03-12 17:40:41 +0900

    First Commit

---
+++ Triple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Triple
+//
+//  Created by nakayasu.yuichi on 2020/03/12.
+//  Copyright © 2020 yuichi.nakayasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TripleTests/TripleTests.swift
@@ -0,0 +1,34 @@
+//
+//  TripleTests.swift
+//  TripleTests
+//
+//  Created by nakayasu.yuichi on 2020/03/12.
+//  Copyright © 2020 yuichi.nakayasu. All rights reserved.
+//
+
+import XCTest
+@testable import Triple
+
+class TripleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
